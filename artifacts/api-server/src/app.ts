8fcba29f5bcd69a9653d9925f3d94e0252755520
Saadia Asghar <saadianigah@gmail.com>
2026-07-13 10:12:50 +0500

    Remove request logger from Vercel API

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,16 +1,9 @@
-import express, { type NextFunction, type Request, type Response } from "express";
+import express from "express";
 import cors from "cors";
 import router from "./routes/index.js";
-import { logger } from "./lib/logger.js";
 
 const app = express();
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.on("finish", () => {
-    logger.info({ method: req.method, statusCode: res.statusCode, url: req.path }, "Request completed");
-  });
-  next();
-});
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
